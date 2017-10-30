e9c59644b62b13afcc2c6b1b7539d3fac79faec7
Kion Smith <kion_smith@hotmail.com>
2017-10-30 13:28:28 -0500

    Basic testing for the bot

--- ChatBot/src/ChatBotMain.java
+++ ChatBot/src/ChatBotMain.java
@@ -1,4 +1,18 @@
 
-public class ChatBotMain {
-
+public class ChatBotMain
+{
+	public static void main(String [] args) 
+	{
+		MyBot b = new MyBot();
+		b.setVerbose(true);
+		try
+		{
+			b.connect("irc.freenode.net");
+		}
+		catch(Exception e)
+		{
+			
+		}
+		b.joinChannel("#picrbot");
+	}
 }
