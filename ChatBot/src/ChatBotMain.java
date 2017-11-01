f7d73282b36424bb6e8cd82bc7f8ae49d6b40901
Kion Smith <kion_smith@hotmail.com>
2017-11-01 17:28:40 -0500

    Trying to get a person to join the server

--- ChatBot/src/ChatBotMain.java
+++ ChatBot/src/ChatBotMain.java
@@ -13,7 +13,7 @@ public class ChatBotMain
 		
 		String channel = "#KionsChatTestRoom";
 		String server  = "irc.freenode.net";
-		Socket socket = new Socket(server, 6667);
+		//Socket socket = new Socket(server, 6667);
 		
 		chatFrame cf = new chatFrame();
 		cf.setTitle("IRC Chat");
@@ -21,7 +21,7 @@ public class ChatBotMain
 		cf.setVisible(true);
 		cf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//b.messages(channel, reader.readLine());
-		
+	/*
 		MyBot b = new MyBot();
 		b.setVerbose(true);
 		try
@@ -34,60 +34,73 @@ public class ChatBotMain
 		}
 		b.joinChannel(channel);
 		//sends current time
+		*/
 		
-		
-		BufferedWriter writer = new BufferedWriter( new OutputStreamWriter(socket.getOutputStream()));
-		BufferedReader reader = new BufferedReader( new InputStreamReader(socket.getInputStream()));
 		
 		//if(reader.equals(""))
 		//{
-			b.onMessage("#KionsChatTestRoom",  "Kion", "test",  "user",  "time");
+			//b.onMessage("#KionsChatTestRoom",  "Kion", "test",  "user",  "time");
 		//}
 		
-		/*
-		 * This is for a users
-		
-		String name = "My name Bot";
-		String loginName = "KionsBot";
-		
-		String channel = "#KionsChatTestRoom";
-		
-		
-	
-		writer.write("Name: " + name);
-		writer.flush();
-		
-		String line = null;
-		while((line = reader.readLine( )) != null)
-		{
-			if(line.indexOf("004") >= 0)
-			{
-				break;
-			}
-			else if(line.indexOf("433") >= 0)
-			{
-				System.out.println("The name is already in use");
-				return;
-			}
-		}
-		
-		writer.write("join"+ channel);
-		writer.flush();
-		
-		while ((line = reader.readLine( )) != null) 
-		{
-            if (line.toLowerCase( ).startsWith("PING ")) 
+        String nick = "person1";
+        String login = "person1";
+
+
+        // The channel which the bot will join.
+      //  String channel = "#irchacks";
+        
+        // Connect directly to the IRC server.
+        Socket socket = new Socket(server, 6667);
+        BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(socket.getOutputStream( )));
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(socket.getInputStream( )));
+        
+        // Log on to the server.
+        writer.write("NICK " + nick + "\r\n");
+        writer.write("USER " + login + " 8 * : Java IRC Hacks Bot\r\n");
+        writer.flush( );
+        
+        // Read lines from the server until it tells us we have connected.
+        String line = null;
+        while ((line = reader.readLine( )) != null)
+        {
+            if (line.indexOf("004") >= 0) 
             {
+                // We are now logged in.
+                break;
+            }
+            else if (line.indexOf("433") >= 0) 
+            {
+                System.out.println("Nickname is already in use.");
+                return;
+            }
+            
+        }
+        
+        // Join the channel.
+        writer.write("JOIN " + channel + "\r\n");
+        writer.flush( );
+        
+        // Keep reading lines from the server.
+        while ((line = reader.readLine( )) != null) 
+        {
+            if (line.equals("ping")) 
+            {
+                // We must respond to PINGs to avoid being disconnected.
                 writer.write("PONG " + line.substring(5) + "\r\n");
                 writer.write("PRIVMSG " + channel + " :I got pinged!\r\n");
+                System.out.println("ran");
                 writer.flush( );
             }
             else 
             {
+                // Print the raw line received by the bot.
                 System.out.println(line);
             }
         }
-		*/
-	}
+    }
+		
+	
 	
 }
